fa8f3c61bdb5a716e8c8376810a3f6579b458a9c
shaurya528 <rajshaurya258@gmail.com>
2026-04-09 17:49:40 +0530

    created data access feature

---
+++ TaskManagerAPI/Module/DataAccess.js
@@ -0,0 +1,37 @@
+
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const _dirname =path.dirname(fileURLToPath(import.meta.url));
+
+const Data_file=path.join (_dirname ,'..','Data.json')
+
+export  const getAllTask=()=>{
+    try{
+     const raw_Data=  readFileSync(Data_file,'utf-8');
+     return JSON.parse(raw_Data)
+    }catch(err){
+        console.error("unable to provide all task");
+        return [];
+    }
+}
+export const getTaskById =(id)=>{
+    try{
+   const raw_Data= getAllTask();
+   return raw_Data.find(u=>(u.id===id))
+    }catch(err){
+       console.error("ubable to get task by id");
+       return [];
+    }
+}
+export const getTaskByStatus=(status)=>{
+try{
+  const raw_data= getAllTask();
+  return raw_data.filter(t=>t.status==status);
+  
+}catch(err){
+    console.error("unable to fetch task by Status")
+    return [];
+}
+}
--- TaskManagerAPI/Server.js
+++ TaskManagerAPI/Server.js
@@ -1,7 +1,18 @@
 import  express from "express";
+import { getAllTask, getTaskById, getTaskByStatus } from "./Module/DataAccess.js";
 const app =  express()
 app.get("/",(req,res)=>{
-    res.json({message:"testing server"})
+    const res_Data= JSON.stringify(getAllTask());
+    res.send(res_Data)
+})
+app.get("/id",(req,res)=>{
+    const res_Data_id=JSON.stringify(getTaskById(3))
+    res.send(res_Data_id);
+})
+app.get("/tasks",(req,res)=>{
+    const status=req.query.status
+    const res_Data_Status=JSON.stringify(getTaskByStatus(status))
+    res.send(res_Data_Status);
 })
 const  PORT=3004;
 app.listen(PORT,()=>{
